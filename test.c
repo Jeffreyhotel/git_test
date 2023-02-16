31dfde04c1775a87b4fdef602c55c87640d6fd64
jeffrey <jeffreychen@alum.ccu.edu.tw>
2023-02-16 19:27:10 +0800

    final & go to the head.

--- test.c
+++ test.c
@@ -4,5 +4,6 @@ int main(void)
     printf("Hello world!! +++I'm base file+++");
     printf("I'm not OK.");
     printf("please disappear.98789");
+    printf("that's ok, we go together!");
     return 0;
 }
