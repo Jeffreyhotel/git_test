43b780c9bd21f15139046df528c328718eab9510
jeffrey <jeffreychen@alum.ccu.edu.tw>
2023-02-16 19:13:51 +0800

    Base file compelet!

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 /* file is empty */
 int main(void)
 {
-    printf("Hello world!! +++I'm not here+++");
+    printf("Hello world!! +++I'm base file+++");
     printf("please disappear");
     return 0;
 }
